b9ff50002ec34f83702acd46fecf2e5392d3718f
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2020-06-29 17:07:39 +0300

    test transition events

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,3 +1,22 @@
 defmodule GeoTask.Factory do
-  
+
+  alias GeoTask.Schema.Task
+  alias GeoTask.Schema.User
+
+  def build(:driver) do
+    %User{token: Ecto.UUID.generate(), role: "driver"}
+  end
+
+  def build(:manager) do
+    %User{token: Ecto.UUID.generate(), role: "manager"}
+  end
+
+  def build(:task) do
+    %Task{}
+  end
+
+  def build(factory_name, attributes) do
+    factory_name |> build() |> struct!(attributes)
+  end
+
 end
---
+++ test/unit/transition_test.exs
@@ -0,0 +1,78 @@
+defmodule GeoTask.TransitionTest do
+  use ExUnit.Case, async: true
+
+  import GeoTask.Factory
+
+  alias GeoTask.Transition
+  alias GeoTask.Schema.Task
+  alias GeoTask.Schema.User
+
+  describe "driver transitions" do
+    test "allowed change from new to assign" do
+
+      task = build(:task, status: "new")
+      driver = build(:driver)
+
+      result = driver |> Transition.event(task)
+
+      assert %Task{driver: driver, status: "assign"} = result
+    end
+
+    test "allowed change from assign to done" do
+
+      task = build(:task, status: "assign")
+      driver = build(:driver)
+
+      result = driver |> Transition.event(task)
+
+      assert %Task{status: "done"} = result
+    end
+  end
+
+  describe "manager transitions" do
+    test "allowed to create new task" do
+      task = build(:task)
+      driver = build(:manager)
+
+      result = driver |> Transition.event(task)
+
+      assert %Task{manager: manager, status: "new"} = result
+    end
+  end
+
+  describe "failed transition" do
+    test "forbidden any other transitions" do
+      task = build(:task, status: "assign")
+      manager = build(:manager)
+
+      result = manager |> Transition.event(task)
+
+      assert result == :unsupported_transition
+    end
+
+    test "forbidden to create new task" do
+      task = build(:task)
+      driver = build(:driver)
+
+      result = driver |> Transition.event(task)
+
+      assert result == :unsupported_transition
+    end
+
+    test "task not found" do
+      driver = build(:driver)
+
+      result = driver |> Transition.event(nil)
+
+      assert result == :task_not_found
+    end
+
+    test "user not found" do
+
+      task = build(:task)
+      result = nil |> Transition.event(task)
+
+      assert result == :user_not_found
+    end
+  end
+end
