a07a6e10e1f3bcc6f13c21e893cee6638f49a7bc
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2020-06-29 15:57:48 +0300

    initial commit

---
+++ lib/geo_task.ex
@@ -0,0 +1,18 @@
+defmodule GeoTask do
+  @moduledoc """
+  Documentation for GeoTask.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeoTask.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geo_task_test.exs
@@ -0,0 +1,8 @@
+defmodule GeoTaskTest do
+  use ExUnit.Case
+  doctest GeoTask
+
+  test "greets the world" do
+    assert GeoTask.hello() == :world
+  end
+end
