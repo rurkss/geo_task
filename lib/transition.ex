0f33eaab8f0e03323ef8241e4ba92173acbdf357
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2020-07-02 18:44:35 +0300

    rebuild transition logic

--- lib/transition.ex
+++ lib/transition.ex
@@ -1,16 +1,14 @@
 defmodule GeoTask.Transition do
 
-  alias GeoTask.Schema.{User, Task}
+  alias GeoTask.Schema.User
 
-  def event(%User{role: "driver"} = user, %Task{status: "new"} = task), do: {:ok, %Task{task | status: "assign", driver: user}}
+  def event(%User{role: "driver"}, "new"), do: {:ok, "assign"}
 
-  def event(%User{role: "driver"}, %Task{status: "assign"} = task), do: {:ok, %Task{task | status: "done"}}
+  def event(%User{role: "driver"}, "assign"), do: {:ok, "done"}
 
-  def event(%User{role: "manager"} = user, %Task{status: nil} = task), do: {:ok, %Task{task | status: "new", manager: user}}
+  def event(%User{role: "manager"}, nil), do: {:ok, "new"}
 
   def event(nil, _), do: {:error, :user_not_found}
-
-  def event(_, nil), do: {:error, :task_not_found}
 
   def event(_, _), do: {:error, :unsupported_transition}
 end
--- test/unit/transition_test.exs
+++ test/unit/transition_test.exs
@@ -8,67 +8,57 @@ defmodule GeoTask.TransitionTest do
   describe "driver transitions" do
     test "allowed change from new to assign" do
 
-      task = build(:task, status: "new")
       driver = build(:driver)
 
-      result = driver |> Transition.event(task)
+      result = driver |> Transition.event("new")
 
-      assert {:ok, %Task{status: "assign"}} = result
+      assert {:ok, "assign"} == result
     end
 
     test "allowed change from assign to done" do
 
-      task = build(:task, status: "assign")
       driver = build(:driver)
 
-      result = driver |> Transition.event(task)
+      result = driver |> Transition.event("assign")
 
-      assert {:ok, %Task{status: "done"}} = result
+      assert {:ok, "done"} == result
     end
   end
 
   describe "manager transitions" do
     test "allowed to create new task" do
-      task = build(:task)
+
       driver = build(:manager)
 
-      result = driver |> Transition.event(task)
+      result = driver |> Transition.event(nil)
 
-      assert {:ok, %Task{manager: manager, status: "new"}} = result
+      assert {:ok, "new"} == result
     end
   end
 
   describe "failed transition" do
     test "forbidden any other transitions" do
-      task = build(:task, status: "assign")
+
       manager = build(:manager)
 
-      result = manager |> Transition.event(task)
+      result = manager |> Transition.event("assign")
 
       assert result == {:error, :unsupported_transition}
     end
 
     test "forbidden to create new task" do
-      task = build(:task)
-      driver = build(:driver)
 
-      result = driver |> Transition.event(task)
-
-      assert result == {:error, :unsupported_transition}
-    end
-
-    test "task not found" do
       driver = build(:driver)
 
       result = driver |> Transition.event(nil)
 
-      assert result == {:error, :task_not_found}
+      assert result == {:error, :unsupported_transition}
     end
 
     test "user not found" do
 
       task = build(:task)
-      result = nil |> Transition.event(task)
+      result = nil |> Transition.event("new")
 
       assert result == {:error, :user_not_found}
     end
