75929389e216baefe52bd1e8048aa9d5481f8a51
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2020-07-02 18:53:40 +0300

    introducing task manager
    task manager is responsable for create/assign/update operation

---
+++ lib/task_manager.ex
@@ -0,0 +1,4 @@
+defmodule GeoTask.TaskAssigner do
+
+  
+end
---
+++ test/integration/task_assigner_test.exs
@@ -0,0 +1,30 @@
+defmodule GeoTask.TaskAssignerTest do
+  use ExUnit.Case, async: true
+
+  import GeoTask.Factory  
+  alias GeoTask.TaskAssigner
+
+  describe "create new task by user" do
+    test "success" do
+    end
+
+    test "failure" do
+    end
+  end
+
+  describe "assign user to the task" do
+    test "success" do
+    end
+
+    test "failure" do
+    end
+  end
+
+  describe "update status of task" do
+    test "success" do
+    end
+
+    test "failure" do
+    end
+  end
+end
